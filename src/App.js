a60a385fe16f2ed23aaa017423a0d623d3a19d6c
mirmahyar <m.mirsamadi@gmail.com>
2020-10-14 18:09:09 +0330

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import Guide from "./Guide"
 import './App.css';
+import javascript from "./javascript.png"; import python from "./python.png"
+import go from "./go.png"; import kotlin from "./kotlin.png"
+
+import Particles from 'react-particles-js';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <React.Fragment>
+
+      <Guide />
+      <div className="images">
+        <a href="#">
+          <img id="python" className="img" src={python} alt="Python" />
         </a>
-      </header>
-    </div>
-  );
+        <a href="#">
+          <img id="go" className="img" src={go} alt="Golang" />
+        </a>
+        <a href="#">
+          <img id="kotlin" className="img" src={kotlin} alt="Kotlin" />
+        </a>
+        <a href="#">
+          <img id="javascript" className="img" src={javascript} alt="javascript" />
+        </a>
+      </div>
+
+      <Particles />
+    </React.Fragment>
+
+
+  )
+
 }
 
 export default App;
---
+++ src/Guide.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const guide = () => {
+    return (
+        <p className="guide">WELCOME TO THE EXCITING WORLD OF PROGRAMMING LANGUAGES!
+        WE BELIEVE THESE LANGUAGES ARE LIKE STARS LIGHTING OUR MODERN LIVES!
+        SELECT ONE LANGUAGE TO READ ABOUT IT!
+        </p>
+    )
+}
+
+export default guide
