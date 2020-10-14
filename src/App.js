31e39063722535f04d41cefae690c9f81735a09a
mirmahyar <m.mirsamadi@gmail.com>
2020-10-14 20:14:55 +0330

    enhancement: pictures are buttons now that open modal

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Guide from "./Guide"
 import './App.css';
 import javascript from "./javascript.png"; import python from "./python.png"
 import go from "./go.png"; import kotlin from "./kotlin.png"
-
 import Particles from 'react-particles-js';
+import Modal from "react-modal"
 
 
 function App() {
 
+
+  const [modalIsOpen, SetModalIsOpen] = useState(false)
+
+
+
   return (
     <React.Fragment>
 
+
+      <button onClick={() => SetModalIsOpen(true)}>open modal</button>
+      <Modal isOpen={modalIsOpen}>
+        <h1> HEY HEY HEY </h1>
+        <button onClick={() => SetModalIsOpen(false)}>Close</button>
+      </Modal>
+
+
+
+
+
       <Guide />
       <div className="images">
-        <a href="#">
+        <button onClick={() => SetModalIsOpen(true)}>
           <img id="python" className="img" src={python} alt="Python" />
-        </a>
-        <a href="#">
+        </button>
+        <button onClick={() => SetModalIsOpen(true)}>
           <img id="go" className="img" src={go} alt="Golang" />
-        </a>
-        <a href="#">
+        </button>
+        <button onClick={() => SetModalIsOpen(true)}>
           <img id="kotlin" className="img" src={kotlin} alt="Kotlin" />
-        </a>
-        <a href="#">
+        </button>
+        <button onClick={() => SetModalIsOpen(true)}>
           <img id="javascript" className="img" src={javascript} alt="javascript" />
-        </a>
+        </button>
       </div>
 
       <Particles />
+
     </React.Fragment>
 
 
