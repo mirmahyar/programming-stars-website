50f15cdb918bb2a5420717acb75764af0b4918d6
mirmahyar <m.mirsamadi@gmail.com>
2020-10-15 11:04:52 +0330

    enhancements, added footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import Guide from "./Guide"
 import './App.css';
-import javascript from "./javascript.png"; import python from "./python.png"
-import go from "./go.png"; import kotlin from "./kotlin.png"
+import javascript from "./javascript.png"; import python from "./python.png";
+import csharp from "./csharp.png"
+import go from "./go.png"; import kotlin from "./kotlin.png"; import java from "./java.png"
 import Particles from 'react-particles-js';
 import Modal from "react-modal"
+import Footer from "./Footer"
 
 
 function App() {
 
-
   const [modalIsOpen, SetModalIsOpen] = useState(false)
+  const [modalIsOpenPython, SetModalIsOpenPython] = useState(false)
+  const [modalIsOpenJava, SetModalIsOpenJava] = useState(false)
+  const [modalIsOpenCsharp, SetModalIsOpenCsharp] = useState(false)
+  const [modalIsOpenGo, SetModalIsOpenGo] = useState(false)
+
 
 
 
@@ -18,7 +24,9 @@ function App() {
     <React.Fragment>
 
 
-      <button onClick={() => SetModalIsOpen(true)}>open modal</button>
+
+
+
       <Modal isOpen={modalIsOpen}>
         <h1> HEY HEY HEY </h1>
         <button onClick={() => SetModalIsOpen(false)}>Close</button>
@@ -30,21 +38,77 @@ function App() {
 
       <Guide />
       <div className="images">
-        <button onClick={() => SetModalIsOpen(true)}>
+
+        <a href="#" onClick={() => SetModalIsOpenPython(true)}>
           <img id="python" className="img" src={python} alt="Python" />
-        </button>
-        <button onClick={() => SetModalIsOpen(true)}>
+        </a>
+        <div >
+          <Modal isOpen={modalIsOpenPython} className="pythonmodal">
+            <h1>Python</h1>
+            <button onClick={() => SetModalIsOpenPython(false)}> close </button>
+          </Modal>
+        </div>
+
+        <a href="#" onClick={() => SetModalIsOpenJava(true)}>
+          <img id="java" className="img" src={java} alt="java" />
+        </a>
+        <Modal isOpen={modalIsOpenJava} style={{
+          overlay: {
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: '#0a0a0a'
+          },
+          content: {
+            position: 'absolute',
+            top: '40px',
+            left: '40px',
+            right: '40px',
+            bottom: '40px',
+            border: '1px solid #ccc',
+            background: '#e3da96',
+
+            overflow: 'auto',
+            WebkitOverflowScrolling: 'touch',
+            borderRadius: '4px',
+            outline: 'none',
+            padding: '20px'
+          }
+        }}>
+          <h1>Java</h1>
+          <button onClick={() => SetModalIsOpenJava(false)}> close </button>
+        </Modal>
+
+        <a href="#" onClick={() => SetModalIsOpenGo(true)}>
           <img id="go" className="img" src={go} alt="Golang" />
-        </button>
-        <button onClick={() => SetModalIsOpen(true)}>
+        </a>
+        <Modal isOpen={modalIsOpenGo}>
+          <h1>Go</h1>
+          <button onClick={() => SetModalIsOpenGo(false)}> close </button>
+        </Modal>
+
+        <a href="#" onClick={() => SetModalIsOpen(true)}>
           <img id="kotlin" className="img" src={kotlin} alt="Kotlin" />
-        </button>
-        <button onClick={() => SetModalIsOpen(true)}>
+        </a>
+        <a href="#" onClick={() => SetModalIsOpen(true)}>
           <img id="javascript" className="img" src={javascript} alt="javascript" />
-        </button>
+        </a>
+
+        <a href="#" onClick={() => SetModalIsOpenCsharp(true)}>
+          <img id="csharp" className="img" src={csharp} alt="csharp" />
+        </a>
+        <Modal isOpen={modalIsOpenCsharp}>
+          <h1>Csharp</h1>
+          <button onClick={() => SetModalIsOpenCsharp(false)}> close </button>
+        </Modal>
+
       </div>
 
       <Particles />
+
+      <Footer />
 
     </React.Fragment>
 
---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Footer = () => {
+    return (
+        <div className='footer' >
+            <h3 id="myfooter">
+                All rights reserved
+
+            Copyright @ 2020  </h3>
+
+        </div>
+    )
+}
+
+
+
+
+
+export default Footer
